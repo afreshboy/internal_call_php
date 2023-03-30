b0c6bf9a6b614aea32fbc7e9119229efe46993c3
songziming.me <songziming.me@bytedance.com>
2023-03-30 21:01:28 +0800

    mongo

--- app/Http/Controllers/BaseReqController.php
+++ app/Http/Controllers/BaseReqController.php
@@ -10,6 +10,10 @@ class BaseReqController extends Controller
     {
         $num1 = $request->input('num1', 0);
         $num2 = $request->input('num2', 0);
+        echo(explode(':', getenv('MONGO_ADDRESS'))[0]);
+        echo(getenv('MONGO_USERNAME'));
+        echo(getenv('MONGO_PASSWORD'));
+        echo(explode(':', explode(',', getenv('MONGO_ADDRESS'))[0])[1]);
         return (int)$num1 + (int)$num2;
     }
 

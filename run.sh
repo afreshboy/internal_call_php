372c207b576283f84e3f2366f903d8e6e79b92db
songziming.me <songziming.me@bytedance.com>
2023-03-31 10:49:20 +0800

    test1

--- run.sh
+++ run.sh
@@ -3,10 +3,6 @@
 # 后台启动
 composer install --no-plugins --no-scripts
 php artisan cache:clear
-php-fpm -D
-if [$? -ne 0]; then
-  echo "fpm init failed"
-fi
 
 # 关闭后台启动，hold住进程
 nginx -g 'daemon off;'
